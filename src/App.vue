1e247a09cef4fd2bd572a3db989d3bd339f00241
Vladimir <v.troshev@itsportal.ru>
2023-07-28 10:39:04 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <FirstSlide></FirstSlide>
+</template>
+
+<script>
+import FirstSlide from '@/components/slides/FirstSllide.vue'
+export default {
+  name: 'App',
+  components: {
+    FirstSlide
+  }
+}
+</script>
+
+<style>
+
+</style>
