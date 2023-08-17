0d2c20f13dd0dde2994541615967d44107f4658b
Vladimir <v.troshev@itsportal.ru>
2023-08-17 13:07:55 +0300

    fix

--- src/components/GameSlider/slides/GameSllide6.vue
+++ src/components/GameSlider/slides/GameSllide6.vue
@@ -1,14 +1,13 @@
 <!-- eslint-disable vue/no-parsing-error -->
 <template>
     <transition-group name="fade" mode="out-in">
-        <firstStep v-show="step >= 0" :user-name="userName" />
-
-        <secondStep v-show="step >= 1" :user-name="userName" />
-
-        <thirdStep v-show="step >= 2" :user-name="userName" />
-
-        <fourthStep v-show="step >= 3" :user-name="userName"/>
+        <firstStep v-if="step === 0" />
+        <secondStep v-if="step === 1" />
+        <thirdStep v-if="step === 2" />
+        <fourthStep v-if="step === 3"/>
     </transition-group>
+
+    <TextsComponent :user-name="userName"></TextsComponent>
 </template>
 
 <script setup>
@@ -19,6 +18,7 @@ import firstStep from '@/components/slide6steps/firstStep.vue'
 import secondStep from '@/components/slide6steps/secondStep.vue'
 import thirdStep from '@/components/slide6steps/thirdStep.vue'
 import fourthStep from '@/components/slide6steps/fourthStep.vue'
+import TextsComponent from '@/components/slide6steps/TextsComponent.vue'
 
 // eslint-disable-next-line no-undef
 const emit = defineEmits(['continue']) 
@@ -27,10 +27,15 @@ const step = ref(0)
 const user = useUserStore()
 const { userName } = storeToRefs(user)
 
+let text1 
+let text2 
+let text3 
+let text4 
+let text5 
+
 const nextStep = () => {
-    const text3 = document.querySelector('#text-3')
-    const text4 = document.querySelector('#text-4')
-    const text5 = document.querySelector('#text-5')
+    
+    text2.classList.remove('visible')
 
     setTimeout(() => { // Мужик назад
         step.value++
@@ -40,20 +45,24 @@ const nextStep = () => {
         text3.classList.add('visible')
     }, 2000)
 
+    setTimeout(() => { // Представила Яну
+        text3.classList.remove('visible')
+    }, 2500)
+
     setTimeout(() => { // Подсветили Яну
         step.value++
     }, 3000)
 
     setTimeout(() => { // Яна
-        text4.classList.add('visible')
+        text5.classList.add('visible')
     }, 4000)
 
     setTimeout(() => { // Яна
-        text4.classList.remove('visible')
+        text5.classList.remove('visible')
     }, 4500)
 
     setTimeout(() => { // Пошли работать
-        text5.classList.add('visible')
+        text4.classList.add('visible')
         emit('continue')
     }, 5000)
 }
@@ -61,13 +70,19 @@ const nextStep = () => {
 onMounted(async () => {
     await nextTick()
 
-    const text1 = document.querySelector('#text-1')
-    const text2 = document.querySelector('#text-2')
-    
+    text1 = document.querySelector('#text-1')
+    text2 = document.querySelector('#text-2')
+    text3 = document.querySelector('#text-3')
+    text4 = document.querySelector('#text-4')
+    text5 = document.querySelector('#text-5')
     
     setTimeout(() => { // первый текст
         text1.classList.add('visible')
     }, 1500)
+
+    setTimeout(() => { // первый текст
+        text1.classList.remove('visible')
+    }, 3000)
 
     setTimeout(() => { // Мужик появился
         step.value++
