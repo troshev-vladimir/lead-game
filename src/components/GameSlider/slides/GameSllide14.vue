8574a4647c1b1f6557b445f70c4372d65482f12d
Vladimir <v.troshev@itsportal.ru>
2023-09-04 12:46:16 +0300

    fix

--- src/components/GameSlider/slides/GameSllide14.vue
+++ src/components/GameSlider/slides/GameSllide14.vue
@@ -1,16 +1,13 @@
 <template>
-    <iframe class="iframe" src="http://max43.ru:9444/IntroProduct?N=Задачи&C=Задача1" width="100%" height="100%" scrolling="auto"> </iframe>
+    <iframe class="iframe" src="https://max43.ru:9444/IntroProduct?N=Задачи&C=Задача1" width="100%" height="100%" scrolling="auto"> </iframe>
 
 
-    <div class="tips" @click="emit('nextSlide')">
-        <iframe class="iframe" src="http://127.0.0.1:5500/src/index.html" width="100%" height="100%" scrolling="auto"> </iframe>
+    <div class="tips">
+        <iframe class="iframe" src="https://lk.itseducation.ru/editor/edd/" width="100%" height="100%" scrolling="auto"> </iframe>
     </div>
 </template>
 
 <script setup>
-// eslint-disable-next-line no-undef
-const emit = defineEmits(['nextSlide'])
-
 </script>
 
 <style>
--- src/store/navigation.js
+++ src/store/navigation.js
@@ -2,22 +2,22 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useNavigationStore = defineStore("navigation", () => {
-	const currentStep = ref(-1);
-	const totalSteps = 18;
+  const currentStep = ref(-1);
+  const totalSteps = 18;
 
-	function stepForward() {
-		if (currentStep.value === totalSteps) return;
-		currentStep.value++;
-	}
+  function stepForward() {
+    if (currentStep.value === totalSteps) return;
+    currentStep.value++;
+  }
 
-	function stepBackward() {
-		if (currentStep.value === 0) return;
-		currentStep.value--;
-	}
+  function stepBackward() {
+    if (currentStep.value === 0) return;
+    currentStep.value--;
+  }
 
-	return {
-		currentStep,
-		stepForward,
-		stepBackward,
-	};
+  return {
+    currentStep,
+    stepForward,
+    stepBackward,
+  };
 });
