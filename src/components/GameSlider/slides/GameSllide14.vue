b504a0d1058950ae34a4c1fb7da50022a88056bf
Vladimir <v.troshev@itsportal.ru>
2023-09-04 13:11:02 +0300

    fix

--- src/components/GameSlider/slides/GameSllide14.vue
+++ src/components/GameSlider/slides/GameSllide14.vue
@@ -1,23 +1,19 @@
 <template>
-    <iframe class="iframe" src="http://max43.ru:9444/IntroProduct?N=Задачи&C=Задача1" width="100%" height="100%" scrolling="auto"> </iframe>
-
-
-    <div class="tips">
-        <iframe class="iframe" src="https://lk.itseducation.ru/editor/edd/" width="100%" height="100%" scrolling="auto"> </iframe>
-    </div>
+    <iframe class="screen" src="http://max43.ru:9444/IntroProduct?N=Задачи&C=Задача1" width="100%" height="100%" scrolling="auto"> </iframe>
+    <iframe class="editor" src="https://lk.itseducation.ru/editor/edd/" width="100%" height="100%" scrolling="auto"> </iframe>
 </template>
 
 <script setup>
 </script>
 
 <style>
-.iframe {
-    width: 70vw;
+.screen {
+    width: 50vw;
     height: 100%;
 }
 
-.tips {
-    width: 30vw;
+.editor {
+    width: 50vw;
     height: 100%;
     padding: 0;
 }
